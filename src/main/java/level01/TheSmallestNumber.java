6fff51d89a91b01873d9d24c19a1c1a22f266ecb
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-12-03 11:58:11 +0900

    Initial commit

---
+++ src/main/java/level01/TheSmallestNumber.java
@@ -0,0 +1,39 @@
+package level01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TheSmallestNumber {
+    public int[] Solution(int[] arr) {
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        }
+
+        int[] answer = new int[arr.length - 1];
+
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min) {
+                continue;
+            }
+            answer[index] = arr[i];
+            index++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        TheSmallestNumber theSmallestNumber = new TheSmallestNumber();
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] result = theSmallestNumber.Solution(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
